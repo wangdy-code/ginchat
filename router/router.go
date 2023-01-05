dd7019c3d370c370bf804a798554e6a99f882ba2
delta <1158046762@qq.com>
2023-01-05 17:33:36 +0800

    添加swager

--- router/router.go
+++ router/router.go
@@ -3,10 +3,15 @@ package router
 import (
 	"ginchat/service"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/basic/docs"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.GET("/index", service.GetIndex)
 	return r
 }
--- service/index.go
+++ service/index.go
@@ -2,6 +2,15 @@ package service
 
 import "github.com/gin-gonic/gin"
 
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} index
+// @Router /index [get]
 func GetIndex(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
